418c65362ebbf38333da9b0a2eca1ef1e656b443
benada002 <45796304+benada002@users.noreply.github.com>
2020-06-22 13:58:17 +0200

    Add Idb

---
+++ src/idb.ts
@@ -0,0 +1,62 @@
+import { openDB, DBSchema } from 'idb';
+
+interface Part {
+    name: string;
+    size: number;
+}
+
+interface Sizes {
+    size: number | string;
+    parts: Array<Part>
+}
+
+interface MaterialCalculatorDB extends DBSchema {
+    products: {
+        value: {
+            id?: number;
+            name: string;
+            parts: Array<string>;
+            sizes: Array<Sizes>;
+        };
+        key: number;
+        indexes: { 'name': string };
+    };
+    materials: {
+        value: {
+            id?: number;
+            name: string;
+            manufacturer: string;
+            width: number;
+            fLength: number;
+            price: number;
+        };
+        key: number;
+        indexes: { 'manufacturer': string };
+    };
+    'settings': {
+        key: string;
+        value: any;
+    };
+}
+
+async function getDB() {
+  return openDB<MaterialCalculatorDB>('MaterialCalculator', 1, {
+    upgrade(db) {
+      const products = db.createObjectStore('products', {
+        keyPath: 'id',
+        autoIncrement: true,
+      });
+      products.createIndex('name', 'name');
+
+      const materials = db.createObjectStore('materials', {
+        keyPath: 'id',
+        autoIncrement: true,
+      });
+      materials.createIndex('manufacturer', 'manufacturer');
+
+      db.createObjectStore('settings');
+    },
+  });
+}
+
+export default getDB;
